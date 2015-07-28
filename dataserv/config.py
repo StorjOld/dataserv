218406650f2276b217c9ac034046f5fad9318bad
Shawn Wilkinson <shawn@storj.io>
2015-07-28 10:28:51 -0400

    Updated Config

--- dataserv/config.py
+++ dataserv/config.py
@@ -1,6 +1,6 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///dataserv.db"  # path to database
 ONLINE_TIME = 15  # minutes
+SQLALCHEMY_DATABASE_URI = "sqlite:///dataserv.db"
 
 DATA_DIR = 'data/'
-BYTE_SIZE = 10*1024*1024  # 10 MB
-BYTE_FARMER_MAX = 30*1024*1024  # 30 MB
+BYTE_SIZE = 1024*1024*128  # 128 MB
+BYTE_FARMER_MAX = 1024*1024*1024  # 1 GB
