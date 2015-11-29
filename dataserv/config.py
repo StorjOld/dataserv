cad5b3413ff73667cfd4cdb243b2649801c59fb5
littleskunk <jens.heimbuerge@googlemail.com>
2015-11-29 18:32:57 +0100

    higher auth timeout for slow cpu
    https://github.com/Storj/dataserv-client/issues/178

--- dataserv/config.py
+++ dataserv/config.py
@@ -25,7 +25,7 @@ BYTE_SIZE = 1024*1024*128  # 128 MB FIXME rename, very confusing name
 HEIGHT_LIMIT = 200000  # around 25 TB
 
 ADDRESS = "16ZcxFDdkVJR1P8GMNmWFyhS4EKrRMsWNG"  # unique per server address
-AUTHENTICATION_TIMEOUT = 20  # seconds
+AUTHENTICATION_TIMEOUT = 30  # seconds
 SKIP_AUTHENTICATION = False  # only for testing
 
 _log_format = "%(asctime)s %(levelname)s %(name)s %(lineno)d: %(message)s"
