8bdd033b9378ccbe4f7ab668a979961587934742
F483 <fabian.barkhau@gmail.com>
2015-12-27 22:44:25 +0100

    updated requirements and start server script

---
+++ start_production_server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# set correct dir
+cd "$(dirname "$0")"
+
+# TODO create env if needed
+# TODO pull latest
+# TODO migrate if needed
+
+# load virtualenv
+source env/bin/activate
+
+# configure
+export DATASERV_DATABASE_URI="postgresql:///dataserv"
+export PYCOIN_NATIVE=openssl
+
+# start server
+cd dataserv
+authbind gunicorn -w 4 -b 0.0.0.0:80 app:app --log-file=gunicorn.log
